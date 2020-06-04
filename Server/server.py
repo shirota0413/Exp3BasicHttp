7b7ffcf102d19766c7cd62520bb2e607f914785b
島袋太智 <e185712@e185712.local>
2020-06-04 12:30:32 +0900

    server.py編集

--- Server/server.py
+++ Server/server.py
@@ -1,8 +1,24 @@
 import socket
-# ホスト名を取得、表示
-host = socket.gethostname()
-print(host)
 
-# ipアドレスを取得、表示
-ip = socket.gethostbyname(host)
-print(ip) # 192.168.○○○.○○○
+host = socket.gethostname()
+PORT = 80
+BUFFER_SIZE = 2048
+count = 0
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((host,PORT))
+    s.listen(1)
+    while True:
+        connection, client = s.accept()
+        print(connection,client)
+        try:
+            header = 'HTTP/1.1 200 OK\n' + 'Hello: BasicHTTP!\n' + 'Content-Length: 13\n' + 'Content-Type: text/plain; charset=utf-8\n'
+            body = (socket.gethostbyname(host) + '\n').encode('utf8')
+
+            connection.send(header.encode('utf8'))
+
+            connection.send(body)
+
+            data = connection.recv(BUFFER_SIZE)
+            connection.send(data.upper())
+        finally:
+            connection.close()
